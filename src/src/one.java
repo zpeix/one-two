5c5367d766d362aa935a99c4222b58d925beea76
zpeix <1553853614@qq.com>
2018-01-03 19:25:13 +0800

    https://github.com/zpeix/one-two.git

---
+++ src/src/one.java
@@ -0,0 +1,25 @@
+package src;
+
+public class one {
+
+
+		// TODO Auto-generated method stub
+		
+		private one(){
+			
+		}
+		public static int getMax(int a,int b){
+			return a>=b?a:b;
+		}
+		public static int getMin(int a,int b){
+			return a<=b?a:b;
+		}
+		public static int add(int a,int b){
+			return a+b;
+		}
+		public static int div(int a,int b){
+			return a-b;
+	}
+	}
+
+
---
+++ src/src/two.java
@@ -0,0 +1,12 @@
+package src;
+
+public class two {
+	public static void main(String[] args) {
+int  a=40;
+int b=20;
+System.out.println(one.add(a,b));
+System.out.println(one.div(a,b));	
+System.out.println(one.getMax(a,b));
+System.out.println(one.getMin(a,b));
+}
+	}
